4dbfda329445ba8a861b450a0689234c4e919cd9
Mariusz Szpala <mariusz.szpala@gmail.com>
2017-04-07 17:29:56 +0200

    Zadanie 8.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
+var name = prompt('Wpisz swoje imię'); // komentarz na końcu linii 
+//alert('Witaj, ' + name);
 console.log('Witaj, ' + name);
+
+
+/*
+
+Komentarz wieloliniowy !!!
+
+*/
